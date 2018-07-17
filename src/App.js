b9411ce1d69eaddc81345f858a97df1dbaa15c01
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-07-17 23:07:01 +0100

    Remove boilerplate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import GoogleMap from './components/google_map';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -11,10 +10,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <div style={{height: 500}}>
           Map me!
           <button onClick={() => this.setState({ lat: 40.7128, lng: -74.005})}>
