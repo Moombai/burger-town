45e023695f001a0b6453087108fd8707dd1af051
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-07-19 21:24:02 +0100

    Static styles for sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Sidebar = () => (
-  <div className="Sidebar"></div>
+  <div className="Sidebar">
+    <div className="container">
+      <input type="text" placeholder="Search by location" />
+      <ul>
+        <li>Venue</li>
+        <li>Venue</li>
+        <li>Venue</li>
+        <li>Venue</li>
+        <li>Venue</li>
+      </ul>
+    </div>
+  </div>
 );
 
 export default Sidebar;
