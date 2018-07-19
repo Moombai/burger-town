f9c72b4bea855def9e890784aeaf7a9f80044a3b
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-07-19 21:54:26 +0100

    Render locations from state

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Sidebar />
+        <Sidebar locations={this.state.locations} />
         <div className="Main">
           <Header />
           {/* Map me!
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const Sidebar = () => (
+const Sidebar = (props) => (
   <div className="Sidebar">
     <div className="container">
       <input type="text" placeholder="Search by location" />
       <ul>
-        <li>Venue</li>
-        <li>Venue</li>
-        <li>Venue</li>
-        <li>Venue</li>
-        <li>Venue</li>
+        {props.locations.map((location, key) => (
+          <li key={key}>{location.title}</li>
+        )) }
       </ul>
     </div>
   </div>
