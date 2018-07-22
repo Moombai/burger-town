36cca9232ed6e9ae90e7aa2acfffd07ec6c3d68e
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-07-22 15:06:58 +0100

    Remove linting message in console

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,23 @@
 import React from 'react';
 
-const Header = (props) => (
-  <div className="Header">
-    <div
-      id="menu"
-      role="button"
-      tabIndex="0"
-      aria-expanded={`${props.extendSidebar}`}
-      onClick={props.handleSidebarToggle}
-      onKeyPress={props.handleSidebarToggle}
-      >
-      <span className="hamburger"></span>
-      <span className="hamburger"></span>
-      <span className="hamburger"></span>
+const Header = (props) => {
+  let status = props.extendSidebar === false ? "false" : "true";
+  return (
+    <div className="Header">
+      <div
+        id="menu"
+        role="button"
+        tabIndex="0"
+        aria-expanded={status}
+        onClick={props.handleSidebarToggle}
+        onKeyPress={props.handleSidebarToggle}
+        >
+        <span className="hamburger"></span>
+        <span className="hamburger"></span>
+        <span className="hamburger"></span>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Header;
