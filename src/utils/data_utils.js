ab5019cf800fe574191620f4d156037565ae0b1d
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2018-07-27 12:39:38 +0100

    Tidy up variable names

--- src/utils/data_utils.js
+++ src/utils/data_utils.js
@@ -1,13 +1,14 @@
 function fetchFromFourSquare() {
   const requestUrl = "https://api.foursquare.com/v2/venues/";
-  const clientId = "VUQYEQENPANR3UEYUMV2TS3A10CIMJ55WB3IL5ZHOWGDQOAP";
-  const clientSecret = "TQU2LWXFBSYUBNGPY4N4Q14BYKO1NWHMWWQGFQ1UQF4HY12D";
-  const version = "20180710";
-  let endpoint1 = `${requestUrl}58188c2b38fab86819349c50?client_id=${clientId}&client_secret=${clientSecret}&v=${version}`;
-  let endpoint2 = `${requestUrl}56372298498eccf54c9b7aef?client_id=${clientId}&client_secret=${clientSecret}&v=${version}`;
-  let endpoint3 = `${requestUrl}557ade61498e2f0253159e93?client_id=${clientId}&client_secret=${clientSecret}&v=${version}`;
-  let endpoint4 = `${requestUrl}4fbabe9be4b0c673b642103c?client_id=${clientId}&client_secret=${clientSecret}&v=${version}`;
-  let endpoint5 = `${requestUrl}4ddbb28e18388dd692371228?client_id=${clientId}&client_secret=${clientSecret}&v=${version}`;
+  const clientId = "client_id=VUQYEQENPANR3UEYUMV2TS3A10CIMJ55WB3IL5ZHOWGDQOAP";
+  const clientSecret = "client_secret=TQU2LWXFBSYUBNGPY4N4Q14BYKO1NWHMWWQGFQ1UQF4HY12D";
+  const version = "v=20180710";
+
+  let endpoint1 = `${requestUrl}58188c2b38fab86819349c50?${clientId}&${clientSecret}&${version}`;
+  let endpoint2 = `${requestUrl}56372298498eccf54c9b7aef?${clientId}&${clientSecret}&${version}`;
+  let endpoint3 = `${requestUrl}557ade61498e2f0253159e93?${clientId}&${clientSecret}&${version}`;
+  let endpoint4 = `${requestUrl}4fbabe9be4b0c673b642103c?${clientId}&${clientSecret}&${version}`;
+  let endpoint5 = `${requestUrl}4ddbb28e18388dd692371228?${clientId}&${clientSecret}&${version}`;
 
   function checkStatus(response) {
     if (response.status === 200) {
